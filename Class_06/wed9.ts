d9357797520362b4af130da2d4ece43a4571419a
Farhan-Zafar-Aziz <139948637+Farhan-Zafar-Aziz@users.noreply.github.com>
2023-08-15 22:10:57 +0500

    Add files via upload

---
+++ Class_06/wed9.ts
@@ -0,0 +1,125 @@
+// // Functions
+// // functions are used to repeat same data 
+// //setting parameters in variable
+
+//  function greet(a:string) {  
+//      let message = `suhna ${a}`;
+//       return message;
+//  }
+// //     // console.log("suhna sindhi");
+// //     // console.log("suhna" + a);//concating data in functions
+// //      //console.log(`suhna ${a}`);
+// // }          //{} block of code
+// // //greet("sindhi");
+// let output =greet("sindhi");//jitni martaba call krengy utni martaba same data print hoga.
+//  console.log(output)
+
+// //Function Examples
+
+// function favrtCar(str:string) {
+//   console.log(`${str}`)
+// }
+// favrtCar("Honda Civic")
+
+// function transform(str:string) {
+//   console.log(`My Favorite car is ${str}`);
+// }
+// transform("Honda Civic")
+
+// function weddingOf(mr:string,mss:string) {
+//   let dulhadulhan=(`Mr.${mr} wedds Mss.${mss} Inshallah one day.`);
+//   return dulhadulhan;
+// }
+// let card =weddingOf("Farhan Zafar Aziz Rajar","Sonia Iqbal Rajar");
+// console.log(card);
+
+
+/*function EvenOdd(num:number,) {
+   if(num % 4 ==0){
+     return "Even Number";
+   }
+ else {
+     return "Odd Number";
+ }
+ }
+ let checkNum = 16;//when we enter odd nmbr ansr will be OddNumber(else Condition)
+ //when we enter Even nmbr ansr will be EvenNumber (if condition)
+ let result =EvenOdd(checkNum);
+ console.log(result)*/
+
+ function EvenOdd(num:number,) {
+  if(num % 3 == 0){
+    return "Even Number";
+  }
+ else {
+  return "Odd Number"
+ }
+}
+ let Num = 12;
+let result =EvenOdd(Num);
+console.log(result);
+
+
+//Loops
+
+
+//for (let a=3;a<3;a++);
+// for (let a=0;a<3;a+2);
+// let a = 0;
+// let increment = a++;a++;
+// console.log(a);
+// for (let a=0;a<3;a++){
+//     console.log("Hello")
+// }
+
+for(let q = 0; q < 4; q++);
+let q = 0;
+let increment = q++;
+console.log(q);
+
+for(let q = 0; q<4; q++){
+console.log("Jiye Sindhi");
+}
+
+ for (let i=0; i<5; i++) {
+     console.log(i);
+ }
+
+ let fruits = ["Apple","banana","Mango","orange","grapes"];
+ for (let i=0; i<4; i++) {//(let i=0; fruits.lenth; i++)
+  console.log(fruits[i])
+ }
+
+ for (let i = 0; i<5; i++){
+  console.log("Loop")
+  }
+
+ for (let j=0; j<4; j++){
+   console.log("Nested Loop")
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
