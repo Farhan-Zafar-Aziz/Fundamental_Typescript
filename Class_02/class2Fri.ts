4dce6bbed4dca2f2f873338f01c8c59556dd711e
Farhan-Zafar-Aziz <139948637+Farhan-Zafar-Aziz@users.noreply.github.com>
2023-08-15 21:52:40 +0500

    Typescript Fundamentals

---
+++ Class_02/class2Fri.ts
@@ -0,0 +1,78 @@
+var firstName = "Hello World";
+//1.Variables      
+console.log(firstName);
+//var message = "Hello Pakistan";
+
+var piaic = "Penaverse Bokks";
+console.log(piaic);
+
+var piaPak = "Air Force";
+console.log(piaPak);
+
+//var myMessage = "Hello Dear Sir";
+
+//let myMessage = "Hello Dear";
+//myMessage="Hello Sir"
+
+//const myMessage = "Hello Aziz";
+
+//console.log(myMessage);
+
+//2.Primitive Data Types
+
+let myMessage = "Hello World"; 
+ myMessage = 123; 
+ myMessage = true;
+
+ myMessage: string ="Student"; 
+let myAge: number = 19;
+let isStudent: boolean = true;
+
+let firstName;
+console.log(firstName)
+
+
+let empty = null
+
+let firstName = "Sadiq";
+let age = 22;
+let message = "Hello my name is" + firstName + "and my age is" + age;
+let message = `Hello my name is ${firstName} and my age is ${age}`;
+console.log(message);
+
+let a = 2
+let b = 3
+let c = a ** b
+console.log(c)
+
+console.log(typeof("3")); (typeof 3)
+let a = 20
+console.log(typeof a)
+//Practice.......
+let a = 2
+let b = 4
+let c = a ** b
+console.log(c)
+
+let g =true
+console.log(typeof g)
+
+let a = "Hello"
+console.log(typeof a)
+
+let b = 22
+console.log(typeof b)
+
+let totalMarks = 5
+let obtainMarks = 4
+let result=(totalMarks/obtainMarks)100*
+console.log(result)
+
+let a =10
+// a = a + 5
+//a+=5
+console.log(a)
+a-=5
+console.log(a)
+
+
