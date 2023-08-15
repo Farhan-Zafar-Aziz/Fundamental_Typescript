12f74385a9b5ca0678aa7e2115a17c9f032962ae
Farhan-Zafar-Aziz <139948637+Farhan-Zafar-Aziz@users.noreply.github.com>
2023-08-15 22:04:38 +0500

    Typescript Fundamentals

---
+++ Class_03/file4.js
@@ -0,0 +1,118 @@
+//Template Literals/Strings
+//A new and fast way to deal with strings is Template Literals or Strings.
+//Eaxample
+/*var myName = "Farhan";
+var hello = "Hello" + myName;
+console.log(hello);
+
+var shanakht = "Pakistan";
+var naaro = "Zindabad" + shanakht;
+console.log(naaro);*/
+//Analyzing And Modifying Data Types
+//you can check the type of variable entering typeof.
+//Example
+/*let testVariable = 1;
+console.log(typeof testVariable);
+
+let var1 = "Umerkot";
+console.log(typeof var1);
+
+let areMarried = false;
+console.log(typeof areMarried);*/
+//The Variables in typescript cannot change types.Example
+//let a 2;
+//a= "2"; Error
+//Operators
+//1.Arithmatic Operators
+//Addition/ Example
+/*let n = 2;
+let n1 = 2;
+console.log(n+n1);
+
+let n2 = 3;
+let n4 = 5;
+let result = n2 + n4;
+console.log(result);
+
+let var1= "2";
+let _var = "2";
+console.log(var1+_var);*/ // Ans 22
+//Substraction/Example
+/*let n1 = 9;
+let n2 = 8;
+console.log(n1 - n2);
+
+let far = 8;
+let nar = 4;
+let result = far - nar;
+console.log(result);*/
+//Multiplication/Example
+/*let $uk = 2;
+let _uk = 5;
+console.log($uk * _uk);
+
+let moto = 6;
+let mota = 5;
+let result = (moto * mota);
+console.log(result);*/
+//Division/Example
+/*let div = 20;
+let div1 = 4;
+console.log(div/div1);
+
+let lat = 50;
+let lEt = 5;
+let result = lat/lEt;
+console.log(result);*/
+//Exponentiation/Example
+/*let n = 3;
+let m = 3;
+console.log(n**m);// 3*3=9*3=27 Ans
+
+let f = 2;
+let g = 4;
+let result = 2 ** 4;//2*4=8, /8*2=16 ans
+console.log(result);*/
+//Modules % /Example
+/*let mod1 = 550;
+let mod2 = 450;
+console.log(mod1%mod2);*/
+//Assignment Operators
+//Assignment operator are used to assigning values to variables./Example
+/*let n = 5;
+n += 5;
+console.log(n);
+n -= 5;
+console.log(n)
+
+let nar1 = 10;
+nar1 += 10;
+nar1 -= 5;
+let result = (nar1);
+console.log(result);*/
+//Comparision Operators
+//Comparision operator are used to compare values of variables./Example
+//let n = 10;
+//console.log(n == 10);/true (==) used to compare values.
+//console.log(n == 9 or 11);/false bcz value is not equal
+//console.log(n === 10);true/(===) uesd to strong compare values.
+//console.log(n === 9 or 11 );/false bcz value is not equal
+//console.log(n != 10);/false (!=) used to ionvert value.
+//console.log(n != 9);/true bcz value is wrong of the variable.
+//let n = 10;
+//console.log(n > 8);/true n value is greater then 8.
+//console.log(n > 11);/false not greater then variables value.
+//console.log(n < 8);/false not less then variables value.
+//console.log(n < 12);/true n value is less then 12.
+//console.log(n >= 11);/false not greater not equal 
+//console.log(n >= 9);/true n value is greater then 9.
+//console.log(n <= 10);/true  n value is equal to variable.
+//Logical operators
+// logical operator are used to combine multiple conditions in one./Example
+
+/*var n = 5;
+//console.log(n >=5 && n < 10);/true bcz n value is = 5 /(&&) it mean (and)
+//console.log(n > 5 && n < 10);/false bcz n value is not greater then 5
+//console.log(n >= 5 || n < 10);/true bcz n value is equal to 5 /(||) it means or
+//console.log(n > 5 || n < 10);/true bcz n value is less then 10 
+console.log(!(n > 10));*/
